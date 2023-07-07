b8ba9a3a6014b270b774f5187da1a0b605087823
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2023-07-07 20:09:56 +0000

    cuore blu

---
+++ static/js/script.js
@@ -0,0 +1,59 @@
+let bottone = document.getElementById("bottone");
+let contenuto = document.getElementById("contenuto");
+
+bottone.addEventListener("click", function() {
+    if (contenuto.style.display === "none") {
+        contenuto.style.display = "block";
+        bottone.style.display = "none";
+    } else {
+        contenuto.style.display = "none";
+    }
+});
+
+let bottone2 = document.getElementById("bottone2");
+let contenuto2 = document.getElementById("contenuto2");
+
+bottone2.addEventListener("click", function() {
+    if (contenuto2.style.display === "none") {
+        contenuto2.style.display = "block";
+        bottone2.style.display = "none";
+    } else {
+        contenuto2.style.display = "none";
+    }
+});
+
+let bottone3 = document.getElementById("bottone3");
+let contenuto3 = document.getElementById("contenuto3");
+
+bottone3.addEventListener("click", function() {
+    if (contenuto3.style.display === "none") {
+        contenuto3.style.display = "block";
+        bottone3.style.display = "none";
+    } else {
+        contenuto3.style.display = "none";
+    }
+});
+
+let bottone4 = document.getElementById("bottone4");
+let contenuto4 = document.getElementById("contenuto4");
+
+bottone4.addEventListener("click", function() {
+    if (contenuto4.style.display === "none") {
+        contenuto4.style.display = "block";
+        bottone4.style.display = "none";
+    } else {
+        contenuto4.style.display = "none";
+    }
+});
+
+let bottone5 = document.getElementById("bottone5");
+let contenuto5 = document.getElementById("contenuto5");
+
+bottone5.addEventListener("click", function() {
+    if (contenuto5.style.display === "none") {
+        contenuto5.style.display = "block";
+        bottone5.style.display = "none";
+    } else {
+        contenuto5.style.display = "none";
+    }
+});
